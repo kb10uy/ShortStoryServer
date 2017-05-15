af19996979f55380fefbefb5a3401c9f0a4b9be2
kb10uy <kb10uy@kb10uy.org>
2017-05-15 22:41:38 +0900

    輪姦コマンドを追加

--- resources/views/layouts/includes-after.blade.php
+++ resources/views/layouts/includes-after.blade.php
@@ -1,4 +1,4 @@
-<script src="//{{ Request::getHost() }}:{{ env('SOCKETIO_PORT') }}/socket.io/socket.io.js"></script>
+<script src="/socket.io/socket.io.js"></script>
 <script type="text/javascript" src="{{ mix('/js/manifest.js') }}"></script>
 <script type="text/javascript" src="{{ mix('/js/vendor.js') }}"></script>
 <script src="https://cdn.jsdelivr.net/emojione/2.2.7/lib/js/emojione.min.js"></script>
--- resources/views/post/view.blade.php
+++ resources/views/post/view.blade.php
@@ -3,13 +3,14 @@
 @section('title', $post->title)
 
 @section('content')
-<div class="row" data-equalizer data-equalize-on="medium" id="postinfo">
-  <div class="small-12 medium-6 large-8 columns" data-equalizer-watch>
+<div class="row" id="postinfo">
+  <div class="small-12 medium-6 large-8 columns">
     <h1>{{ $post->title }}</h1>
-    <div>
+    <p>
       @include('post.post-info')
-    </div>
-    @include('post.post-tags')
+      @include('post.post-tags')
+    </p>
+    
   </div>
   <div class="small-12 medium-6 large-4 columns">
     <div class="card">
@@ -22,31 +23,38 @@
         </div>
       </div>
     </div>
-    @if(Auth::check())
-      <nice-button tlnice="{{ __('view.post.nice') }}" tlnice_ok="{{ __('view.post.nice_ok') }}" :id="{{ $post->id }}"></nice-button>
-      <dopyulicate-button tldopyu="シコりメールを送る" tldopyu_ok="送信されました！" :id="{{ $post->id }}"></dopyulicate-button>
-      @if(Auth::user() == $post->user)
-        <button class="dropdown button" data-toggle="author-menu">作者メニュー</button>
-        <div class="dropdown-pane bottom right" id="author-menu" data-dropdown>
-          <ul class="vertical dropdown menu" data-dropdown-menu>
-            <li><a href="{{ route('post.edit', ['id' => $post->id]) }}">編集</a></li>
-            <!-- <li><a href="#">非公開にする</a></li> -->
-            <li><a href="#" data-toggle="delete-modal">@lang('view.post.delete')</a></li>
-            @component('layouts.modal-confirm')
-              @slot('title', __('view.post.delete'))
-              @slot('message', '一度削除した作品は復元することはできません。本当に削除してもよろしいですか？')
-              @slot('confirm', __('view.post.delete'))
-              @slot('id', 'delete-modal')
-              @slot('method', 'delete')
-              @slot('action', route('post.delete', ['id' => $post->id]))
-            @endcomponent
-          </ul>
-        </div>
-      @endif
-    @else
-      <a class="button primary" href="{{ route('login') }}">ログインして評価する</a>
-    @endif
   </div>
+</div>
+<div class="row">
+  <p class="text-center"><a data-toggle="post-menu">Toggle Panel</a></p>
+  <div class="small-12 columns"><div class="callout" id="post-menu" data-toggler data-animate="hinge-in-from-top spin-out">
+    <div class="button-group small">
+      @if(Auth::check())
+        <nice-button tlnice="{{ __('view.post.nice') }}" tlnice_ok="{{ __('view.post.nice_ok') }}" :id="{{ $post->id }}"></nice-button>
+        <dopyulicate-button tldopyu="シコりメールを送る" tldopyu_ok="送信されました！" :id="{{ $post->id }}"></dopyulicate-button>
+        @if(Auth::user() == $post->user)
+          <button class="dropdown button" data-toggle="author-menu">作者メニュー</button>
+          <div class="dropdown-pane bottom right" id="author-menu" data-dropdown>
+            <ul class="vertical dropdown menu" data-dropdown-menu>
+              <li><a href="{{ route('post.edit', ['id' => $post->id]) }}">編集</a></li>
+              <!-- <li><a href="#">非公開にする</a></li> -->
+              <li><a href="#" data-toggle="delete-modal">@lang('view.post.delete')</a></li>
+              @component('layouts.modal-confirm')
+                @slot('title', __('view.post.delete'))
+                @slot('message', '一度削除した作品は復元することはできません。本当に削除してもよろしいですか？')
+                @slot('confirm', __('view.post.delete'))
+                @slot('id', 'delete-modal')
+                @slot('method', 'delete')
+                @slot('action', route('post.delete', ['id' => $post->id]))
+              @endcomponent
+            </ul>
+          </div>
+        @endif
+      @else
+        <a class="button primary" href="{{ route('login') }}">ログインして評価する</a>
+      @endif
+    </div>
+  </div></div>
 </div>
 <hr>
 <div class="row">
